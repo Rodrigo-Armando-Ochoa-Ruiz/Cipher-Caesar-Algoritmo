e7ffa6ac22a630508814198360ec5b5dba71baa3
Rodrigo Ochoa <rodrigo.ochoa.ruiz.19@gmail.com>
2024-09-17 18:30:09 -0600

    Feat: Se agregó otro alfabeto

--- src/Cipher.java
+++ src/Cipher.java
@@ -2,7 +2,15 @@ public class Cipher {
     private static final char [] ALPHABET={'a','b','c','d','e','f','g','h','i',//9
                                             'j','k','l','m','n','o','p', 'q','r',//9
                                             's','t','u','v','w','x','y','z',//8 T=26
-                                            '.', ',', '"', ':', '!', '?', ' ','\n'};//7
+                                            '.', ',', '"', ':', '!', '?', ' ','\n',
+                                            'á','é','í','ó','ú','ñ',
+                                            '(',')'};//7
+    private static final char [] WIREDALPHABET={1040,1041,1042,1043,1044,1045,1046,1047,1048,
+                                                1049,1050,1051,1052,1053,1054,1055,1056,1057,
+                                                1058,1059,1060,1061,1062,1063,1064,1065,
+                                                '.', ',', '"', ':', '!', '?', ' ','\n',
+                                                'á','é','í','ó','ú','ñ',
+                                                '(',')'};
     private static Cipher instance;
 
     private Cipher() {
@@ -15,19 +23,39 @@ public class Cipher {
         return instance;
     }
 
-    public static String cipher(String text, int key){
+    public static String encrypt(String text, int key){
         StringBuilder cipherText=new StringBuilder();
         char [] toCipher=text.toLowerCase().toCharArray();
         int newPosition;
         for (char c : toCipher) {
             for (int j = 0; j < ALPHABET.length; j++) {
                 if (c > 96 && c < 123 && c == ALPHABET[j]) {
-                    newPosition =(j+key>=0)?(j + key) % 26:26+(j+key);
-                    cipherText.append(ALPHABET[newPosition]);
+                    newPosition =(j + key) % 26;
+                    cipherText.append(WIREDALPHABET[newPosition]);
                     break;
                 }
                 if (c == ALPHABET[j]) {
                     cipherText.append(ALPHABET[j]);
+                    break;
+                }
+            }
+        }
+        return cipherText.toString();
+    }
+
+    public static String decrypt(String text, int key){
+        StringBuilder cipherText=new StringBuilder();
+        char [] toCipher=text.toCharArray();
+        int newPosition;
+        for (char c : toCipher) {
+            for (int j = 0; j < WIREDALPHABET.length; j++) {
+                if (c > 1039 && c < 1066 && c == WIREDALPHABET[j]) {
+                    newPosition =(j+key>=0)?(j + key) % 26:26+(j+key);
+                    cipherText.append(ALPHABET[newPosition]);
+                    break;
+                }
+                if (c == WIREDALPHABET[j]) {
+                    cipherText.append(WIREDALPHABET[j]);
                     break;
                 }
             }
--- src/FileProcess.java
+++ src/FileProcess.java
@@ -13,12 +13,10 @@ public class FileProcess {
 
         Path srcPath = Paths.get(inputPath);
         Path destPath = Paths.get(outputPath);
-
+        String cipherText;
         //Definición de canales de entrada y salida de datos
         try (FileChannel inputChannel=FileChannel.open(srcPath, StandardOpenOption.READ);
              FileChannel outputChannel=FileChannel.open(destPath, StandardOpenOption.WRITE)){
-
-
 
             ByteBuffer byteBufferRead =ByteBuffer.allocate(1024); //Definir Buffer de lectura con tamaño de información a procesar
             ByteBuffer byteBufferWrite; //Definir buffer de escritura
@@ -31,7 +29,10 @@ public class FileProcess {
                 byteBufferRead.get(bufferBytesRead);//Guardar los datos del buffer de lectura en el arreglo
 
                 //Enviar a Cifrado la clave y la cadena de texto, guardar texto cifrado
-                String cipherText =Cipher.cipher(new String(bufferBytesRead,StandardCharsets.UTF_8),key);
+                if (key>0)
+                    cipherText =Cipher.encrypt(new String(bufferBytesRead,StandardCharsets.UTF_8),key);
+                else
+                    cipherText =Cipher.decrypt(new String(bufferBytesRead,StandardCharsets.UTF_8),key);
 
                 //Convertir el texto cifrado a bytes para su escritura
                 byteBufferWrite =ByteBuffer.wrap(cipherText.getBytes(StandardCharsets.UTF_8));
@@ -47,5 +48,7 @@ public class FileProcess {
         }catch (IOException e) {
             System.out.println(e.getMessage()+"\n"+e.getCause()+"\n"+ Arrays.toString(e.getStackTrace()));
         }
+
+        System.out.println("Proceso realizado con éxito");
     }
 }
